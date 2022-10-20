8943d91b8292b761a6372a972763060ef6446577
EightCosmos <parksj38@icloud.com>
2022-10-21 00:02:13 +0900

    firs commit

---
+++ TetrisGame/Function.swift
@@ -0,0 +1,17 @@
+//
+//  Function.swift
+//  TetrisGame
+//
+//  Created by SJ PARK on 2022/10/20.
+//
+
+import Foundation
+import SpriteKit
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello")
+            .padding()
+    }
+}
